6a2b1287c9ff5ffa46253b70008fb90653deb208
jeancarlos0 <jecarlos.me@gmail.com>
2020-08-07 22:37:55 -0300

    Corrigido bug em createConnection

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -26,21 +26,21 @@ export default class ClassesController{
         //cconverte a hora dada em minutos, recebe o valor como uma string
         const timeInMinutes = convertHourToMinutes(time);
         //`classes`.`id` vem do classes da primeira listagem
-        const classes = await db('classes')
-        .whereExists(function(){
-            this.select('class_schedule.*')
-            .from('class_schedule')
-            .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-            .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-            .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-            .whereRaw('`class_schedule`.`from` > ??', [timeInMinutes])
-        })
-        .where('classes.subject', '=', subject)
-        .join('users', 'classes.user_id', '=', 'users.id')
-        .select(['classes.*', 'users.*']);
+        const classes = await db("classes")
+            .whereExists(function () {
+                this.select("class_schedule.*")
+                .from("class_schedule")
+                .whereRaw("`class_schedule`.`class_id` = `classes`.`id`")
+                .whereRaw("`class_schedule`.`week_day` = ??", [Number(week_day)])
+                .whereRaw("`class_schedule`.`from` <= ??", [timeInMinutes])
+                .whereRaw("`class_schedule`.`to` > ??", [timeInMinutes]);
+            })
+            .where("classes.subject", "=", subject)
+            .join("users", "classes.user_id", "=", "users.id")
+            .select("classes.*", "users.*");
 
-        return response.json(classes);
-    }
+            return response.json(classes);
+        }
 
     async create(request: Request, response: Response){
         const {
